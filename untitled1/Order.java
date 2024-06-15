697abcd85844c910f4bfe78ff1099eb28a72527b
Li Hua <3396481554@qq.com>
2024-06-16 01:43:38 +0800

    Create Order.java

---
+++ untitled1/Order.java
@@ -0,0 +1,66 @@
+package com.ssm.pojo;
+public class Order {
+
+    private int id;
+    private String number;
+    private String member_id;
+    private String product_name;
+    private Integer count;
+    private Double money;
+    private String addTime;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getMember_id() {
+        return member_id;
+    }
+
+    public void setMember_id(String member_id) {
+        this.member_id = member_id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    public String getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(String addTime) {
+        this.addTime = addTime;
+    }
+}
